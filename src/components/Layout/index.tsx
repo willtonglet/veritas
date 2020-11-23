8919907f6b621500947b0cd5bba60d33c495649b
William Tonglet <william.tonglet@wipro.com>
2020-11-23 15:21:16 -0300

    insere favicon

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -13,7 +13,6 @@ const Layout: React.FC<PageProps> = ({ children, content }) => {
         <>
             <Head>
                 <title>Veritás - Vila Madalena</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="Description" content="Veritás - Vila Madalena"></meta>
             </Head>
             <AppContext content={content}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '@core/styles/global.scss';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, minimum-scale=1, viewport-fit=cover"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
 export default MyApp;
