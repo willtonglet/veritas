40b759c64775571e11a6ff1dbcffcc1d773a6875
William Tonglet <william.tonglet@wipro.com>
2020-11-23 11:07:10 -0300

    insere font no head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ class MyDocument extends Document {
     render(): JSX.Element {
         return (
             <Html lang="pt-BR">
-                <Head />
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
