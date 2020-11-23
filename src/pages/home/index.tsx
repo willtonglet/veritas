a370e56d74bf2c500945230668e39ab79c197b5f
William Tonglet <william.tonglet@wipro.com>
2020-11-23 11:40:23 -0300

    organiza imports

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,15 +7,10 @@ import Cover from '@sections/cover';
 import Info from '@sections/info';
 import Empreendimento from '@sections/empreendimento';
 import Plantas from '@sections/plantas';
-
 import Perspectivas from '@sections/perspectivas';
-
 import Localizacao from '@sections/localizacao';
-
 import LocalizacaoMapa from '@sections/localizacao-mapa';
-
 import Video from '@sections/video';
-
 import Contato from '@sections/contato';
 
 const Home: NextPage<PageProps> = (props) => {
