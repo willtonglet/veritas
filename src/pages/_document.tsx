be9888afece185baee0831bfe24960c38c5fc155
William Tonglet <william.tonglet@wipro.com>
2020-11-23 10:41:53 -0300

    insere font no scss para teste

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,7 @@ class MyDocument extends Document {
     render(): JSX.Element {
         return (
             <Html lang="pt-BR">
-                <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap"
-                        rel="stylesheet"
-                    />
-                </Head>
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
