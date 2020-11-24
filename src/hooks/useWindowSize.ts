7b9b1032c2d4a14a5db41d9cfcd3120665ce49af
William Tonglet <william.tonglet@wipro.com>
2020-11-24 14:46:59 -0300

    insere number no useWindowSize

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 export default function useWindowSize(): number {
-    const [windowSize, setWindowSize] = useState();
+    const [windowSize, setWindowSize] = useState(0);
 
     useEffect(() => {
         function handleResize() {
